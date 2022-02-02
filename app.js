b2b8bda002cf05f3b997a05b6cfdb994da71157f
Statcomm <63654894+Statcomm@users.noreply.github.com>
2022-02-02 16:05:35 +0300

    done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDb = require('./database');
 const productsRoutes = require('./api/products/routes');
+const path = require("path")
 
 const app = express();
 connectDb();
@@ -12,16 +13,17 @@ app.use(express.json());
 app.use((req, res, next) => {
   console.log(
     `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
-  );
-  next();
-});
-
+    );
+    next();
+  });
+  
 // Routes
 app.use('/products', productsRoutes);
+app.use("/media", express.static(path.join(__dirname,"media")))
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message || 'Internal Server Error',
   });
 });
-app.listen(process.env.PORT || 5000);
+app.listen(8000);
